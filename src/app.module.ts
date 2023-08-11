40a533878005908ec7687d946135f819b44ca94c
Omid <omidk7914.ok@gmail.com>
2023-08-11 10:52:09 +0430

    create resource User

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 import datasource from './shared/db/Datasource';
 @Module({
   imports: [ProductModule,TypeOrmModule.forRoot({
     ...datasource.options,
     autoLoadEntities: true,
-  })],
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
