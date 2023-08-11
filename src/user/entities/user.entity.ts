0d85f7c81c0f556446d5683aa596bc9d8ed53b5d
Omid <omidk7914.ok@gmail.com>
2023-08-11 11:13:31 +0430

    create user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,39 @@
-export class User {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class UsersEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ nullable: true })
+  family: string;
+
+  @Column()
+  age: string;
+
+  @Column({ nullable: true })
+  cellphone: string;
+
+  @Column({ nullable: true })
+  birthdate: number;
+
+  @Column()
+  active: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+
+  @Column()
+  deletedAt: Date;
+
+}
