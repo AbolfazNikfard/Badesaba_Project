788aae8958262906c9051c0dbd4c9bae7a0a2eb2
Omid <omidk7914.ok@gmail.com>
2023-08-11 12:01:17 +0430

    hash passwod whit bceptjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,15 +2,18 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { registerDto } from './dto/register.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService:UserService){}
-  register(registerDto: registerDto) {
-    const user = this.userService.findByEmail(registerDto.email)
+  async register(registerDto: registerDto) {
+    const user = await this.userService.findByEmail(registerDto.email)
     if(user){
       throw new HttpException('user already exists', 400)
     }
+    registerDto.password = await bcrypt.hash(registerDto.password , 10)
+    return await this.userService.create(registerDto)
   }
 
 
